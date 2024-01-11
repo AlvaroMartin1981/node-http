70cffd3c4d5a65a644f1aa84fef1219983f1ebd8
titopope@hotmail.com <titopope@hotmail.com>
2024-01-11 12:44:55 +0100

    terminado

---
+++ data.js
@@ -0,0 +1,7 @@
+const data={
+    title:'Navidad',
+    subtitle: 'Vacaciones de invierno',
+    description: 'Niños jugando con los regalos de papa Noel y los reyes magos'
+}
+
+module.exports = data
---
+++ server.js
@@ -0,0 +1,28 @@
+const http = require('node:http');
+const data = require('./data.js');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+
+  const htmlContent = `<!DOCTYPE html>
+    <html lang="es">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Navidad</title>
+      </head>
+      <body>
+        <h1>${data.title}</h1>
+        <h2>${data.subtitle}</h2>
+        <p>${data.description}</p>
+      </body>
+    </html>`;
+
+  res.end(htmlContent);
+});
+
+server.listen(0, () => {
+  console.log(
+    `Server listening on port http://localhost:${server.address().port}`
+  );
+});
